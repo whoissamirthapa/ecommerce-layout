04024f8bd6506b36bb90d22d6ba2cb4bf0a48b0a
Samir Thapa <59677197+thapasamirm577@users.noreply.github.com>
2023-01-06 07:13:57 +0545

    Update footer.js

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -42,16 +42,16 @@ const Footer = () => {
                 <article className='follow_us'>
                     <h2>Follow us on</h2>
                     <div>
-                        <a href='#' target={"_blank"}>
+                        <a href='https://facebook.com' target={"_blank"}>
                             <FaFacebookSquare className="footer_icon_social" />
                         </a>
-                        <a href='#' target={"_blank"}>
+                        <a href='https://twitter.com' target={"_blank"}>
                             <FaTwitterSquare className="footer_icon_social" />
                         </a>
-                        <a href='#' target={"_blank"}>
+                        <a href='https://instagram.com' target={"_blank"}>
                             <FaInstagramSquare className="footer_icon_social" />
                         </a>
-                        <a href='#' target={"_blank"}>
+                        <a href='https://linkedin.com/in/feed' target={"_blank"}>
                             <FaLinkedin className="footer_icon_social" />
                         </a>
                     </div>
